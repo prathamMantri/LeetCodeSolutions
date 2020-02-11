5ad8d76f13b34d8cc75d315e901d4f6dc902971a
Prathamesh Mantri <pmantri@cccis.com>
2020-02-11 10:29:33 -0600

    P-12345: Microsoft More solutions
    Signed-off-by: Prathamesh Mantri <pmantri@cccis.com>

---
+++ src/microsoftInterviewQuestions/LargestInteger.java
@@ -0,0 +1,38 @@
+package microsoftInterviewQuestions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://leetcode.com/discuss/interview-question/406031/
+ */
+
+public class LargestInteger {
+
+    public static void main(String[] args) {
+
+        LargestInteger sol = new LargestInteger();
+        int[] input = {3, 2, -2, 5, -3};
+        int[] input2 = {1, 2, 3, -4};
+        System.out.println(sol.solution(input));
+        System.out.println(sol.solution(input2));
+
+
+    }
+
+    public int solution(int[] input) {
+        int max = 0;
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < input.length; i++) {
+            list.add(input[i]);
+        }
+
+        for (int i = 0; i < input.length; i++) {
+            if (list.contains(input[i] * (-1))) {
+                max = Math.abs(input[i]) > max ? Math.abs(input[i]) : max;
+            }
+        }
+        return max;
+
+    }
+}
