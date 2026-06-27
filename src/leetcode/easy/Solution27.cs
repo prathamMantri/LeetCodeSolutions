8541295486b9f8bbd27c72863605a767604b015a
Prathamesh Mantri <pratham.mantri@gmail.com>
2026-06-27 16:20:57 -0700

    Implement RemoveElement method in Solution27
    https://leetcode.com/problems/remove-element/

---
+++ src/leetcode/easy/Solution27.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public int RemoveElement(int[] nums, int val) {
+
+        if(nums.Length ==  0)
+        {
+            return 0;
+        }
+
+        int index = 0;
+        int lastIndex = nums.Length - 1;
+        
+        while(index <= lastIndex)
+        {
+            if(nums[lastIndex] == val)
+            {
+                lastIndex--;
+                continue;
+            }
+            if(nums[index] == val)
+            {
+                nums[index] = nums[lastIndex--];
+            }
+            index++;
+        }
+        return index;
+    }
+}
