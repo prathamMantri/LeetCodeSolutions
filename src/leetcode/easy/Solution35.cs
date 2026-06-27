622b948ee455b83733c238aba7d866eeee5850d0
Prathamesh Mantri <pratham.mantri@gmail.com>
2026-06-27 16:18:38 -0700

    Implement SearchInsert method in Solution35 class
    https://leetcode.com/problems/search-insert-position/

---
+++ src/leetcode/easy/Solution35.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public int SearchInsert(int[] nums, int target) {
+        int i=0;
+        int j= nums.Length-1;
+
+        while(i<=j)
+        {
+            int mid = i + (j-i)/2;
+            if(target == nums[mid])
+            {
+                return mid;
+            }
+            else if(target < nums[mid])
+            {
+                j = mid-1;
+            }
+            else
+            {
+                i = mid+1;
+            }
+        }
+        return i;
+    }
+}
