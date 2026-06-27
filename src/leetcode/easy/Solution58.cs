fff0aa188a0964c227d5dc3c1bfa527ce8c76143
Prathamesh Mantri <pratham.mantri@gmail.com>
2026-06-27 16:17:10 -0700

    Implement LengthOfLastWord method in Solution58

---
+++ src/leetcode/easy/Solution58.cs
@@ -0,0 +1,21 @@
+public class Solution58 {
+    public int LengthOfLastWord(string s) {
+        if(string.IsNullOrEmpty(s))
+        {
+            return 0;
+        }
+        
+        string trimmed = s.Trim();
+
+        int end = trimmed.Length-1;
+
+        int count = 0;
+
+        while(end >= 0 && trimmed[end] != ' ')
+        {
+            end--;
+            count++;
+        }
+        return count;
+    }
+}
